f24f867d6a24953fdcfa57cb64eb2a8f83e9a093
zekeriyabasan <zksbsn@outlook.com>
2021-06-16 00:34:53 +0300

    Order klasörüne Order service i eklendi katmanlar Order klasörü altında class library olarak oluşturuldu ->Domain - Infrastructure - Aplication <- Order entity si ve migration class ı oluşturuldu

---
+++ ESourcing.Order/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Order.Controllers
+{
+    [Route("api/v1/[controller]")] // metodlara nasıl erişeceğimiz attribute u route
+    [ApiController]
+    public class OrderController : ControllerBase // Api controller olacak o yüzden controllerbase den inherit atmalı
+    {
+     
+    }
+}
---
+++ Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,57 @@
+﻿using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext)
+        {
+            if (!orderContext.orders.Any())
+            {
+                orderContext.orders.AddRange(GetPreconfiguredOrders());
+
+                await orderContext.SaveChangesAsync();
+            }
+        }
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                },
+                                new Order
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test1@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                },
+                                                new Order
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test2@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                }
+
+            };
+        }
+
+    }
+}
